b0532512cba96b203ca11c09a65e33198f6efd8f
Yashraj221b <97579137+YashrajGhule@users.noreply.github.com>
2023-01-24 20:52:24 +0530

    Client-Server Communication Protocol implemented
    This commit contains following implementations and bug fixes:
    1. Implementation of MediaDownload Server-Client communication Protocol on both client and server side
    2. Migrated to Selenium version 4.7.2
    3. Fixed issues in GetSongInfo.py
    4. Updated MusicDatabase.db with some songs
    5. Added new file named SongInfoSearchData which contains information about songs for future use
    6  Fixed bug #6

--- Controllers/PlayController.cs
+++ Controllers/PlayController.cs
@@ -2,18 +2,55 @@ using System.Net.Sockets;
 using Microsoft.AspNetCore.Mvc;
 using MusicDatabase.Data;
 using MusicDatabase.Models;
+using System.Security.Cryptography;
 using System.Text;
 
 namespace MusicDatabase.Controllers;
 
 public class PlayController : Controller
 {
+    #pragma warning disable 
+    string INVALID_CODE = "100";
+    string START_DOWNLOAD = "101";
+    string END_DOWNLOAD = "102";
+    string URL_START = "103";
+    string URL_END = "104";
+    string URL_OK = "105";
+    string URL_ERROR = "106";
+    string HASH_START = "107";
+    string HASH_END = "108";
+    string HASH_OK = "109";
+    string HASH_ERROR = "110";
+    string DOWNLOAD_MEDIA = "111";
+    string DOWNLOAD_COMPLETE = "112";
+    string DOWNLOAD_ERROR = "113";
+    string TOKEN_SUCCESS = "114";
+    string TOKEN_ERROR = "115";
+
+    #pragma warning restore
+
     private MusicContext database = new MusicContext();
     private readonly ILogger<HomeController> _logger;
 
     public PlayController(ILogger<HomeController> logger)
     {
         _logger = logger;
+    }
+    
+    private string ToHex(byte[] bytes, bool upperCase)
+    {
+        StringBuilder result = new StringBuilder(bytes.Length * 2);
+        for (int i = 0; i < bytes.Length; i++)
+            result.Append(bytes[i].ToString(upperCase ? "X2" : "x2"));
+        return result.ToString();
+    }
+
+    private byte[] GenerateToken(byte[] data)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            return sha256.ComputeHash(data);
+        }
     }
 
     public IActionResult Index(string? mid)
@@ -86,23 +123,80 @@ public class PlayController : Controller
         {
             if (database.songEntries != null)
             {
-                var server = new TcpClient();
+                var server = new TcpClient(AddressFamily.InterNetwork);
                 server.Connect("localhost", 8731);
                 var stream = server.GetStream();
+
+                byte[] code = new byte[3];
+
+                byte[] serverToken = new byte[64];
+                stream.Read(serverToken, 0, 64);
+                byte[] clientToken = Encoding.ASCII.GetBytes(ToHex(GenerateToken(serverToken),false));
+                stream.Write(clientToken);
+
+                stream.Read(code, 0, 3);
+
+                if (Encoding.ASCII.GetString(code) != TOKEN_SUCCESS)
+                {
+                    stream.Close();
+                    server.Close();
+                    server.Dispose();
+                    return Content("<h1>ERROR OCCURED</h1>\n<h2>Invalid Token</h2>", "text/html");
+                }
+                stream.Write(Encoding.ASCII.GetBytes(START_DOWNLOAD));
+                
                 foreach (var song in database.songEntries)
                 {
                     if (song.SongEntryId != null && song.SongURL != null)
                     {
-                        Console.WriteLine("DOWNLOADING :" + song.SongEntryId);
-                        stream.Write(Encoding.ASCII.GetBytes(song.SongURL));
-                        // stream.Write(Encoding.ASCII.GetBytes("|"));
+                        Console.WriteLine("Name: " + song.SongName);
+                        Console.WriteLine("URL: " + song.SongURL);
+
+                        stream.Write(Encoding.ASCII.GetBytes(URL_START));
                         Thread.Sleep(1000);
-                        stream.Write(Encoding.ASCII.GetBytes(song.SongEntryId));
-                        var data = new Byte[1024];
-                        var bytes = stream.Read(data, 0, data.Length);
-                        Console.WriteLine(Encoding.ASCII.GetString(data, 0, bytes));
+
+                        stream.Write(Encoding.ASCII.GetBytes(song.SongURL));
+                        Thread.Sleep(1000);
+
+                        stream.Write(Encoding.ASCII.GetBytes(URL_END));
+                        Thread.Sleep(1000);
+
+                        stream.Read(code, 0, 3);
+                        Thread.Sleep(1000);
+                        
+                        if (Encoding.ASCII.GetString(code) == URL_OK)
+                        {
+                            stream.Write(Encoding.ASCII.GetBytes(HASH_START));
+                            Thread.Sleep(1000);
+
+                            stream.Write(Encoding.ASCII.GetBytes(song.SongEntryId));
+                            Thread.Sleep(1000);
+
+                            stream.Write(Encoding.ASCII.GetBytes(HASH_END));
+                            Thread.Sleep(1000);
+
+                            stream.Read(code, 0, 3);
+                            Thread.Sleep(1000);
+
+                            if (Encoding.ASCII.GetString(code) == HASH_OK)
+                            {
+                                stream.Write(Encoding.ASCII.GetBytes(DOWNLOAD_MEDIA));
+                                Thread.Sleep(1000);
+                                stream.Read(code, 0, 3);
+                                Thread.Sleep(1000);
+                                if (Encoding.ASCII.GetString(code) == DOWNLOAD_COMPLETE)
+                                {
+                                    Console.WriteLine("DOWNLOADED: " + song.SongURL);
+                                }
+                                else if(Encoding.ASCII.GetString(code) == DOWNLOAD_ERROR)
+                                {
+                                    Console.WriteLine("ERROR DOWNLOADING: " + song.SongURL);
+                                }
+                            }
+                        }
                     }
                 }
+                stream.Write(Encoding.ASCII.GetBytes(END_DOWNLOAD));
                 stream.Close();
                 server.Close();
             }
--- DownloadServer.py
+++ DownloadServer.py
@@ -1,16 +1,24 @@
 import hashlib
 import socket
 import threading
+from pytube import YouTube
 
-INVALID_CODE = b"-1"
-START_DOWNLOAD = b"102"
-END_DOWNLOAD = b"108"
+INVALID_CODE = b"100"
+START_DOWNLOAD = b"101"
+END_DOWNLOAD = b"102"
 URL_START = b"103"
 URL_END = b"104"
-HASH_START = b"105"
-HASH_END = b"106"
-DOWNLOAD_MEDIA = b"107"
-DOWNLOAD_COMPLETE = b"108"
+URL_OK = b"105"
+URL_ERROR = b"106"
+HASH_START = b"107"
+HASH_END = b"108"
+HASH_OK = b"109"
+HASH_ERROR = b"110"
+DOWNLOAD_MEDIA = b"111"
+DOWNLOAD_COMPLETE = b"112"
+DOWNLOAD_ERROR = b"113"
+TOKEN_SUCCESS = b"114"
+TOKEN_ERROR = b"115"
 
 
 def generateToken(hashable: bytes) -> str:
@@ -21,35 +29,55 @@ def handleConnection(client: socket.socket, address: tuple):
     code = client.recv(3)
     if code == START_DOWNLOAD:
 
-        url = None
-        hash = None
+        url = b""
+        hash = b""
 
         while True:
+            print("Listening for code..")
             code = client.recv(3)
+            print("Code received: ",code)
             if code == URL_START:
                 url = client.recv(2048)
 
             elif code == URL_END:
                 url = url.decode("utf-8")
+                if url != None and url != "":
+                    client.send(URL_OK)
+                else:
+                    client.send(URL_ERROR)
 
             elif code == HASH_START:
                 hash = client.recv(2048)
 
             elif code == HASH_END:
                 hash = hash.decode("utf-8")
+                if url != None and url != "":
+                    client.send(HASH_OK)
+                else:
+                    client.send(HASH_ERROR)
 
             elif code == DOWNLOAD_MEDIA:
-                # TODO
-                client.send(DOWNLOAD_COMPLETE)
+                if url == None or url == "":
+                    client.send(URL_ERROR)
+                elif hash == None or hash == "":
+                    client.send(HASH_ERROR)
+                else:
+                    try:
+                        media = YouTube(url)
+                        media.streams.get_audio_only().download(output_path="./Songs/",filename=hash+".mp4")
+                        client.send(DOWNLOAD_COMPLETE)
+                    except:
+                        client.send(DOWNLOAD_ERROR)
 
             elif code == END_DOWNLOAD:
                 client.close()
                 break
+            else:
+                client.send(INVALID_CODE);
 
     else:
         client.send(INVALID_CODE)
         client.close()
-    print("Disconnected: ",address)
 
 
 def establishConnection(client: socket.socket, address: tuple):
@@ -60,31 +88,32 @@ def establishConnection(client: socket.socket, address: tuple):
         client_token = client.recv(1024)
 
         if client_token == generateToken(server_token.encode()).encode():
+            client.send(TOKEN_SUCCESS)
             handleConnection(client, address)
         else:
+            client.send(TOKEN_ERROR)
+            client.shutdown(socket.SHUT_RDWR)
             client.close()
     except ConnectionAbortedError:
         print("ConnectionAborted:", address)
+    except ConnectionResetError:
+        print("ConnectionReset:", address)
     except ConnectionError:
-        print("Connection:", address)
-    except Exception as e:
-        print(e)
+        print("ConnectionError:", address)
     print("Disconnected: ",address)
 
 
 def main():
+    host, port = ("127.0.0.1", 8731)
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((host, port))
+    server.listen(10)
     while True:
-        host, port = ("127.0.0.1", 221)
-        server = socket.socket()
-        server.bind((host, port))
-
-        server.listen(10)
-
         client, address = server.accept()
         print("Connected: ",address)
         thread = threading.Thread(
             target=establishConnection, args=(client, address))
-        thread.daemon = True
+        thread.daemon = False
         thread.start()
 
 if __name__ == "__main__":
